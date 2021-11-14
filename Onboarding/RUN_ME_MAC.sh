06f07fb7e46dd4656c7fd11fb5180283ce238096
Sergey Klimov <sergey.v.klimov@gmail.com>
2021-11-14 14:54:17 +0200

    Update RUN_ME_MAC.sh

--- Onboarding/RUN_ME_MAC.sh
+++ Onboarding/RUN_ME_MAC.sh
@@ -14,5 +14,20 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # [Mosh](mosh.org)
 # [NPM](npmjs.org)
 # [Browsh][brow.sh]
-brew install mosh npm browsh git zsh  
-brew install --cask gitkraken gh github
+brew install mosh npm browsh git zsh gh 
+brew install --cask gitkraken  github
+
+mkdir DEV
+cd DEV
+gh auth login
+
+# ? What account do you want to log into? GitHub.com
+# ? What is your preferred protocol for Git operations? SSH
+# ? Generate a new SSH key to add to your GitHub account? Yes
+# ? Enter a passphrase for your new SSH key (Optional) 
+# ? How would you like to authenticate GitHub CLI? Login with a web browser
+
+# ! First copy your one-time code: 6520-D692
+# - Press Enter to open github.com in your browser... 
+# ✓ Authentication complete. Press Enter to continue...
+gh repo clone PMExtra/GitCracken
