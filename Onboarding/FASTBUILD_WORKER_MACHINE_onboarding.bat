038b037a862f7bdac7099a557fc1297caad00d1c
Sergey Klimov <sergey.v.klimov@gmail.com>
2021-11-16 04:11:57 +0200

    Update FASTBUILD_WORKER_MACHINE_onboarding.bat

--- Onboarding/FASTBUILD_WORKER_MACHINE_onboarding.bat
+++ Onboarding/FASTBUILD_WORKER_MACHINE_onboarding.bat
@@ -1,4 +1,6 @@
-setx FASTBUILD_BROKERAGE_PATH "\\a2vision\FASTBuild"	:: set environment variable
+net use Z: \\alienware\FASTBuild "" /user:Any
+
+setx FASTBUILD_BROKERAGE_PATH "\\alienware\FASTBuild"	:: set environment variable
 setx FASTBUILD_CACHE_PATH "c:\CACHE_FastBuild"
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v FASTBuildWorker /d FBuildWorker.exe	:: start with windows
 start FBuildWorker.exe	:: run build worker
